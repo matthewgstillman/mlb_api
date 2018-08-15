2b3c5ec2b2b7c4de4cbea038faa01796ed0fa0ca
Matt <matthewgstillman@gmail.com>
2018-08-15 14:26:51 -0700

    Latest Commit

--- apps/mlb_api/views.py
+++ apps/mlb_api/views.py
@@ -11,3 +11,33 @@ def index(request):
         'games': games
     }
     return render(request, 'mlb_api/index.html', context)
+
+# def result(request):
+#     if request.method == 'POST':
+#         year = request.POST['year']
+#         request.session['year'] = year
+#         session_year = request.session['year']
+#         month = request.POST['month']
+#         request.session['month'] = month
+#         session_month = request.session['month']
+#         day = request.POST['day']
+#         request.session['day'] = day
+#         session_day = request.session['day']
+#         date = (session_year, session_month, session_day)
+#         date = "(" + str(date[0]) + ", " + str(date[1]) + ", " + str(date[2]) + ")"
+#         print("Date: " + str(date))
+#         games = mlbgame.day(str(date[0]), str(date[2]), str(date[1]))
+#         for game in games:
+#             print game
+#         context = {
+#             'session_year': session_year,
+#             'session_month': session_month,
+#             'session_day': session_day,
+#             'date': date,
+#         }
+#         return render(request, 'mlb_api/result.html',context)
+#     else:
+#         date = "(" + str(date[0]) + ", " + str(date[1]) + ", " + str(date[2]) + ")"
+#         print("Date: " + str(date))
+#         print("Date: " + str(date))
+#         return render(request, 'mlb_api/result.html',context)
